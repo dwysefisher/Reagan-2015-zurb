3d09d66e4cb43a017c52e0ea5656cb154a562f24
Dustin Wyse-Fisher <dwysefisher@eureka.edu>
2015-08-26 15:22:07 -0500

    creating contact forms with Perch

--- contact.php
+++ contact.php
@@ -1,5 +1,3 @@
- 
-
 <?php include('perch/runtime.php');?>
 <!DOCTYPE html>
 <html class="no-js" lang="">
@@ -110,149 +108,154 @@ if($_POST["message"]) {
     mail("web@eureka.edu", "Form to email message", $_POST["message"], "From: [senderEmail]");
 }
 ?>
+
+<div class="row">
+ <div class="small-10 centered columns">
+  <?php perch_content('contact'); ?>
+ </div>
+</div>
+
    <div class="row">
-
-
         <div class="large-9 columns">
 
-          <h3>Get in Touch!</h3>
-          <p>We invite those who share the <em>ReaganForward</em> vision to join us as we advance and build on the leadership legacy of Ronald W. Reagan&hellip;at Eureka College. Sign up below to join our online email and enewsletter list.</p>
+         <h3>Get in Touch!</h3>
+         <p>We invite those who share the <em>ReaganForward</em> vision to join us as we advance and build on the leadership legacy of Ronald W. Reagan&hellip;at Eureka College. Sign up below to join our online email and enewsletter list.</p>
 
-          <div class="section-container tabs" data-section>
-            <section class="section">
-              <h5 class="title">Sign up for the Reagan Society emails</h5>
-              <div class="content" data-slug="panel1">
-                <form action="" method="post">
-                  <div class="row collapse">
-                    <div class="large-2 columns">
-                      <label class="right">Your Name</label>
-                    </div>
-                    <div class="large-10 columns">
-                      <input type="text" id="yourName" name="your_name" placeholder="Ronald Reagan">
-                    </div>
-                  </div>
-                  <div class="row collapse">
-                  <div class="large-2 columns"><label class="right"> Business/organization (if applicable)</label>
-                 </div>
-                  <div class="large-10 columns">
-                    <input type="text" id="business" placeholder="Eureka College">
-                  </div>
-                  </div>
-
-                  <div class="row collapse">           
-                  <div class="large-2 columns">
-                  <label class="right"> Title</label>
-                 </div>
-                  <div class="large-10 columns">
-                    <input type="text" id="title" placeholder="">
-                  </div>
-                  </div>
-
-                  <div class="row collapse">
-                    <div class="large-2 columns">
-                      <label class="right"> Email</label>
-                    </div>
-                    <div class="large-10 columns">
-                      <input type="email" id="senderEmail" placeholder="rreagan@eureka.edu">
-                    </div>
-                  </div>
-
-                  <div class="row collapse">
-                    <div class="large-2 columns">
-                      <label class="right"> Phone</label>
-                    </div>
-                    <div class="large-10 columns">
-                      <input type="tel" id="Phone" placeholder="309-123-4567">
-                    </div>
+         <div class="section-container tabs" data-section>
+           <section class="section">
+             <h5 class="title">Sign up for the Reagan Society emails</h5>
+             <div class="content" data-slug="panel1">
+               <form action="" method="post">
+                 <div class="row collapse">
+                   <div class="large-2 columns">
+                     <label class="right">Your Name</label>
                    </div>
+                   <div class="large-10 columns">
+                     <input type="text" id="yourName" name="your_name" placeholder="Ronald Reagan">
+                   </div>
+                 </div>
+                 <div class="row collapse">
+                 <div class="large-2 columns"><label class="right"> Business/organization (if applicable)</label>
+                </div>
+                 <div class="large-10 columns">
+                   <input type="text" id="business" placeholder="Eureka College">
+                 </div>
+                 </div>
 
-                   <div class="row collapse">
-                    <div class="large-2 columns">
-                     <label> Street Address</label>
-                    </div>
-                    <div class="large-10 columns">
-                      <input type="text" id="streetAddress">
-                    </div>
-                  </div>
+                 <div class="row collapse">
+                 <div class="large-2 columns">
+                 <label class="right"> Title</label>
+                </div>
+                 <div class="large-10 columns">
+                   <input type="text" id="title" placeholder="">
+                 </div>
+                 </div>
 
-                  <div class="row">
-                    <div class="large-4 columns">
-                      <div class="small-12 columns">
-                      <label>City<input type="text" id="city"></label>
-                      </div>
-                    </div>
-                    <div class="large-4 columns">
-                      <div class="small-6 columns">
-                        <label>State<select type="dropdown" id="state" name="state">
-                          <option value="AL">Alabama</option>
-                          <option value="AK">Alaska</option>
-                          <option value="AZ">Arizona</option>
-                          <option value="AR">Arkansas</option>
-                          <option value="CA">California</option>
-                          <option value="CO">Colorado</option>
-                          <option value="CT">Connecticut</option>
-                          <option value="DE">Delaware</option>
-                          <option value="DC">District of Columbia</option>
-                          <option value="FL">Florida</option>
-                          <option value="GA">Georgia</option>
-                          <option value="HI">Hawaii</option>
-                          <option value="ID">Idaho</option>
-                          <option value="IL">Illinois</option>
-                          <option value="IN">Indiana</option>
-                          <option value="IA">Iowa</option>
-                          <option value="KS">Kansas</option>
-                          <option value="KY">Kentucky</option>
-                          <option value="LA">Louisiana</option>
-                          <option value="ME">Maine</option>
-                          <option value="MD">Maryland</option>
-                          <option value="MA">Massachusetts</option>
-                          <option value="MI">Michigan</option>
-                          <option value="MN">Minnesota</option>
-                          <option value="MS">Mississippi</option>
-                          <option value="MO">Missouri</option>
-                          <option value="MT">Montana</option>
-                          <option value="NE">Nebraska</option>
-                          <option value="NV">Nevada</option>
-                          <option value="NH">New Hampshire</option>
-                          <option value="NJ">New Jersey</option>
-                          <option value="NM">New Mexico</option>
-                          <option value="NY">New York</option>
-                          <option value="NC">North Carolina</option>
-                          <option value="ND">North Dakota</option>
-                          <option value="OH">Ohio</option>
-                          <option value="OK">Oklahoma</option>
-                          <option value="OR">Oregon</option>
-                          <option value="PA">Pennsylvania</option>
-                          <option value="RI">Rhode Island</option>
-                          <option value="SC">South Carolina</option>
-                          <option value="SD">South Dakota</option>
-                          <option value="TN">Tennessee</option>
-                          <option value="TX">Texas</option>
-                          <option value="UT">Utah</option>
-                          <option value="VT">Vermont</option>
-                          <option value="VA">Virginia</option>
-                          <option value="WA">Washington</option>
-                          <option value="WV">West Virginia</option>
-                          <option value="WI">Wisconsin</option>
-                          <option value="WY">Wyoming</option>
-                        </select>
-                        </label>
-                      </div>
-                    </div>
-                    <div class="large-4 columns">
-                      <div class="small-6 columns">
-                        <label>Zip Code<input type="text"></label>
-                      </div>  
-                    </div>
+                 <div class="row collapse">
+                   <div class="large-2 columns">
+                     <label class="right"> Email</label>
+                   </div>
+                   <div class="large-10 columns">
+                     <input type="email" id="senderEmail" placeholder="rreagan@eureka.edu">
+                   </div>
+                 </div>
+
+                 <div class="row collapse">
+                   <div class="large-2 columns">
+                     <label class="right"> Phone</label>
+                   </div>
+                   <div class="large-10 columns">
+                     <input type="tel" id="Phone" placeholder="309-123-4567">
+                   </div>
                   </div>
 
-                  </div>
-                  </div>
-                  <button type="submit" class="radius button">Submit</button>
-                </form>
-              </div>
-            </section>
-          </div>
+                  <div class="row collapse">
+                   <div class="large-2 columns">
+                    <label> Street Address</label>
+                   </div>
+                   <div class="large-10 columns">
+                     <input type="text" id="streetAddress">
+                   </div>
+                 </div>
+
+                 <div class="row">
+                   <div class="large-4 columns">
+                     <div class="small-12 columns">
+                     <label>City<input type="text" id="city"></label>
+                     </div>
+                   </div>
+                   <div class="large-4 columns">
+                     <div class="small-6 columns">
+                       <label>State<select type="dropdown" id="state" name="state">
+                         <option value="AL">Alabama</option>
+                         <option value="AK">Alaska</option>
+                         <option value="AZ">Arizona</option>
+                         <option value="AR">Arkansas</option>
+                         <option value="CA">California</option>
+                         <option value="CO">Colorado</option>
+                         <option value="CT">Connecticut</option>
+                         <option value="DE">Delaware</option>
+                         <option value="DC">District of Columbia</option>
+                         <option value="FL">Florida</option>
+                         <option value="GA">Georgia</option>
+                         <option value="HI">Hawaii</option>
+                         <option value="ID">Idaho</option>
+                         <option value="IL">Illinois</option>
+                         <option value="IN">Indiana</option>
+                         <option value="IA">Iowa</option>
+                         <option value="KS">Kansas</option>
+                         <option value="KY">Kentucky</option>
+                         <option value="LA">Louisiana</option>
+                         <option value="ME">Maine</option>
+                         <option value="MD">Maryland</option>
+                         <option value="MA">Massachusetts</option>
+                         <option value="MI">Michigan</option>
+                         <option value="MN">Minnesota</option>
+                         <option value="MS">Mississippi</option>
+                         <option value="MO">Missouri</option>
+                         <option value="MT">Montana</option>
+                         <option value="NE">Nebraska</option>
+                         <option value="NV">Nevada</option>
+                         <option value="NH">New Hampshire</option>
+                         <option value="NJ">New Jersey</option>
+                         <option value="NM">New Mexico</option>
+                         <option value="NY">New York</option>
+                         <option value="NC">North Carolina</option>
+                         <option value="ND">North Dakota</option>
+                         <option value="OH">Ohio</option>
+                         <option value="OK">Oklahoma</option>
+                         <option value="OR">Oregon</option>
+                         <option value="PA">Pennsylvania</option>
+                         <option value="RI">Rhode Island</option>
+                         <option value="SC">South Carolina</option>
+                         <option value="SD">South Dakota</option>
+                         <option value="TN">Tennessee</option>
+                         <option value="TX">Texas</option>
+                         <option value="UT">Utah</option>
+                         <option value="VT">Vermont</option>
+                         <option value="VA">Virginia</option>
+                         <option value="WA">Washington</option>
+                         <option value="WV">West Virginia</option>
+                         <option value="WI">Wisconsin</option>
+                         <option value="WY">Wyoming</option>
+                       </select>
+                       </label>
+                     </div>
+                   </div>
+                   <div class="large-4 columns">
+                     <div class="small-6 columns">
+                       <label>Zip Code<input type="text"></label>
+                     </div>
+                   </div>
+                 </div>
+
+                 </div>
+                 </div>
+                 <button type="submit" class="radius button">Submit</button>
+               </form>
+             </div>
+           </section>
+        </div>
         </div>
 
         <div class="large-3 columns">
---
+++ perch_v2.8.8/perch/config/config.php
@@ -0,0 +1,22 @@
+<?php
+    define('PERCH_LICENSE_KEY', 'P21503-ZLU631-UCA487-VMT720-QMS004');
+
+    define("PERCH_DB_USERNAME", 'reagan15');
+    define("PERCH_DB_PASSWORD", 'M!KpZh%cU0Sa4sh');
+    define("PERCH_DB_SERVER", "reagan15.db.10698437.hostedresource.com");
+    define("PERCH_DB_DATABASE", "reagan15");
+    define("PERCH_DB_PREFIX", "perch2_");
+    
+    define('PERCH_TZ', 'UTC');
+
+    define('PERCH_EMAIL_FROM', 'web@eureka.edu');
+    define('PERCH_EMAIL_FROM_NAME', 'Dustin Wyse-Fisher');
+
+    define('PERCH_LOGINPATH', '/perch');
+    define('PERCH_PATH', str_replace(DIRECTORY_SEPARATOR.'config', '', __DIR__));
+    define('PERCH_CORE', PERCH_PATH.DIRECTORY_SEPARATOR.'core');
+
+    define('PERCH_RESFILEPATH', PERCH_PATH . DIRECTORY_SEPARATOR . 'resources');
+    define('PERCH_RESPATH', PERCH_LOGINPATH . '/resources');
+    
+    define('PERCH_HTML5', true);
